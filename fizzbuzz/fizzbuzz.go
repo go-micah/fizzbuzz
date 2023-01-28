48d2a6ca808aac0b13a68db24e566eb62bb2e8a8
Micah Walter <47419+micahwalter@users.noreply.github.com>
2023-01-28 02:27:28 +0000

    docs

--- fizzbuzz/fizzbuzz.go
+++ fizzbuzz/fizzbuzz.go
@@ -1,7 +1,9 @@
+// Package fizzbuzz allows a program to calculate the answer to FizzBuzz when given any integer
 package fizzbuzz
 
 import "strconv"
 
+// Calculate retusn a string representation of the answer to FizzBuzz given an integer value
 func Calculate(x int) string {
 
 	var multipleOfThree float64 = float64(x) / 3

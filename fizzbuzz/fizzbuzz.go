75c607a8d54d6314fec2b0dd5a4c1ef7b6dc8290
Micah Walter <47419+micahwalter@users.noreply.github.com>
2023-01-28 02:28:21 +0000

    typo

--- fizzbuzz/fizzbuzz.go
+++ fizzbuzz/fizzbuzz.go
@@ -3,7 +3,7 @@ package fizzbuzz
 
 import "strconv"
 
-// Calculate retusn a string representation of the answer to FizzBuzz given an integer value
+// Calculate returns a string representation of the answer to FizzBuzz given an integer value
 func Calculate(x int) string {
 
 	var multipleOfThree float64 = float64(x) / 3
